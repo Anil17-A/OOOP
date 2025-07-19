812bafd494e006cd0073cf0fc3292507a334cf19
Anil <aavulaanil17@gmail.com>
2025-07-19 11:35:49 +0530

    Add files via upload

---
+++ Basicprogrames/src/mypackage/Examples.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class Examples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String greeting = "Hello, World!";
+System.out.println(greeting);
+	}
+
+}
